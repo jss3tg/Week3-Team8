4d9c8e896ae0ef584fa9d27ee4c9cc597e473b4f
deborahjyu <djy3b@virginia.edu>
2022-06-07 17:43:57 -0400

    fixed a bug

--- frontend/src/stripe/CheckoutForm.js
+++ frontend/src/stripe/CheckoutForm.js
@@ -1,7 +1,9 @@
 import React from "react";
 import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
 import axios from "axios";
-import './CheckoutForm.css'
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './CheckoutForm.css';
 
 export const CheckoutForm = () => {
   const stripe = useStripe();
@@ -15,38 +17,35 @@ export const CheckoutForm = () => {
     });
 
     if (!error) {
-        console.log("Stripe 23 | token generated!", paymentMethod);
-        try {
-          const { id } = paymentMethod;
-          const response = await axios.post(
-            "http://localhost:8080/stripe/charge",
-            {
-              amount: 999,
-              id: id,
-            }
-          );
-  
-          console.log("Stripe 35 | data", response.data.success);
-          if (response.data.success) {
-            console.log("CheckoutForm.js 25 | payment successful!");
+      console.log("Stripe 23 | token generated!", paymentMethod);
+      try {
+        const { id } = paymentMethod;
+        const response = await axios.post(
+          "http://localhost:8080/stripe/charge",
+          {
+            amount: 999,
+            id: id,
           }
-        } catch (error) {
-          console.log("CheckoutForm.js 28 | ", error);
+        );
+
+        console.log("Stripe 35 | data", response.data.success);
+        if (response.data.success) {
+          console.log("CheckoutForm.js 25 | payment successful!");
         }
-      } else {
-        console.log(error.message);
+      } catch (error) {
+        console.log("CheckoutForm.js 28 | ", error);
       }
-    };
+    } else {
+      console.log(error.message);
+    }
+  };
+
   return (
-    <><div>
-          <h1 className='checkoutHeader'>Checkout</h1>
-      </div>
-      <form onSubmit={handleSubmit} style={{ maxWidth: 400 }}>
-              <CardElement />
-              <div className = 'payment'>
-              <button type="button" class="btn btn-dark">Pay</button>
-              </div>
-          </form>
-          </>
+    <>
+    <h1 className = 'checkoutHeader'>Checkout</h1>
+    <form onSubmit={handleSubmit} style={{ maxWidth: 400 }}>
+      <CardElement />
+      <Button type='submit' class="btn btn-dark">Pay</Button>
+    </form></>
   );
 };
