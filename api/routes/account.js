35b964eb571b40d6daaed22f588974b599ce38a9
PretzelOptician <alekschultz1@gmail.com>
2022-06-07 16:31:37 -0400

    login screen almost done, all api calls set up

--- api/routes/account.js
+++ api/routes/account.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const app = require("../firebase")
 
 const {getFirestore, getDocs, getDoc, collection, setDoc, doc} = require("firebase/firestore");
-const { createUserWithEmailAndPassword, getAuth } = require("firebase/auth");
+const { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword } = require("firebase/auth");
 
 const db = getFirestore(app);
 const auth = getAuth(app); 
@@ -22,5 +22,23 @@ router.post('/create', async (req, res, next) => {
         })
     }catch(error){console.log(error.message)}
 });
+
+router.get("/login/:email/:password", async (req, res, next) => {
+    try{
+        console.log(req.params)
+        await signInWithEmailAndPassword(auth, req.params.email, req.params.password)
+        const compID = req.params.email.split("@")[0];
+        console.log(compID)
+        let userID = "USER NOT FOUND"
+        const docs = await getDocs(collection(db, "users"))
+        docs.forEach((doc) => {
+            console.log(doc.data().computingId)
+            if(doc.data().computingId == compID) {
+                res.send({id: doc.id, ...doc.data()})
+            }
+        })
+        
+    }catch(error){console.log(error.message)}
+})
 
 module.exports = router; 
---
+++ frontend/src/components/Login.js
@@ -0,0 +1,59 @@
+import axios from "axios";
+import React from "react";
+import {useState} from "react"; 
+
+const Login = () => {
+    const [loginEmail, setLoginEmail] = useState(""); 
+    const [loginPassword, setLoginPassword] = useState(""); 
+    const [signupEmail, setSignupEmail] = useState(""); 
+    const [signupPassword, setSignupPassword] = useState(""); 
+    const [username, setUsername] = useState(""); 
+    const [statusText, setStatusText] = useState(""); 
+    const login = (compID, pass) => {
+        console.log("loggin in")
+        const email = compID + "@virginia.edu";
+        fetch("http://localhost:9000/account/login/" + email + "/" + pass).then((res) => res.json()).then((text) => console.log(text))
+    }
+    const register = (compID, pass, username) => {
+        const email = compID + "@virginia.edu"; 
+        axios.post("http://localhost:9000/account/create", {
+            email: email,
+            password: pass,
+            username: username
+        }).then(() => setStatusText("Signed up! You may now log in."))
+    }
+    return(
+        <div className='login-signup-form'>
+            <div className="login">
+            <h1>Login</h1>
+            <input type="text" placeholder="Enter Computing ID" value={loginEmail} onChange={(e)=>{{
+                console.log(e.target.value); 
+                setLoginEmail(e.target.value); 
+            }}}/>
+            <br></br>
+            <input type="password" placeholder="Enter Password" value={loginPassword} onChange={(e)=>{setLoginPassword(e.target.value)}}/>
+            <br></br>
+            <button onClick={() => {login(loginEmail, loginPassword)}} className='login-btn'>Sign In</button>
+            <p></p>
+            </div>
+
+            <div className="signup">
+            <h1>Create an Account</h1>
+            <input type="text" placeholder="Enter Computing ID" value={signupEmail} onChange={(e)=>{{
+                console.log(e.target.value); 
+                setSignupEmail(e.target.value); 
+            }}}/>
+            <br></br>
+            <input type="password" placeholder="Enter Password" value={signupPassword} onChange={(e)=>{setSignupPassword(e.target.value)}}/>
+            <br></br>
+            <input type="text" placeholder="Enter Username" value={username} onChange={(e)=>{setUsername(e.target.value)}}/>
+            <br></br>
+            <button onClick={() => {register(signupEmail, signupPassword, username)}} className='login-btn'>Sign Up</button>
+            <p></p>
+            </div>
+            <p>{statusText}</p>
+        </div>
+    );
+}
+
+export default Login
