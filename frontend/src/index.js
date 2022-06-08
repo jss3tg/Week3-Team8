8e19aa207d81cd57044b6323bf27b6081ae1b7c5
jss3tg <jss3tg@virginia.edu>
2022-06-08 10:39:54 -0400

    heroku again

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import { BrowserRouter as Router } from "react-router-dom";
 import reportWebVitals from './reportWebVitals';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import './bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
