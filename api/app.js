e527f567d9561289eaca752a1583689a8fa49862
jss3tg <jss3tg@virginia.edu>
2022-06-08 10:33:55 -0400

    heroku stuff

--- api/app.js
+++ api/app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 const productsRouter = require("./routes/products");
@@ -11,6 +12,7 @@ const accountsRouter = require("./routes/account");
 require('dotenv').config()
 
 var app = express();
+app.use(express.static(path.join(__dirname, '../frontend/build')));
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
@@ -29,6 +31,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
