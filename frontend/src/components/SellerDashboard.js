fee0971dd961060cf1f156a36bd989b29a71a98b
jss3tg <jss3tg@virginia.edu>
2022-06-08 11:57:14 -0400

    heroku deploy

--- frontend/src/components/SellerDashboard.js
+++ frontend/src/components/SellerDashboard.js
@@ -2,23 +2,27 @@ import React from "react";
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container'
+import Form from 'react-bootstrap/Form';
 import './SellerDashboard.css';
 
 function SellerDashboard() {
   return (
-    <Container className = "container">
-      <Card className = 'seller-card' >
-      <Card.Img variant="top" src="image" />
-        <Card.Body>
-          <Card.Title>Wahoo Shirt</Card.Title>
-          <Card.Text>
-            Looking to sell multiple old shirts. 
-            Price: $15
-          </Card.Text>
-          <Button variant="primary">Buy</Button>
-        </Card.Body>
-      </Card>
-    </Container>
+    <>
+      <Form.Group className="mb-3">
+        <Form.Label>Disabled input</Form.Label>
+        <Form.Control placeholder="Disabled input" disabled />
+      </Form.Group>
+      <Form.Group className="mb-3">
+        <Form.Label>Disabled select menu</Form.Label>
+        <Form.Select disabled>
+          <option>Disabled select</option>
+        </Form.Select>
+      </Form.Group>
+      <Form.Group className="mb-3">
+        <Form.Check type="checkbox" label="Can't check this" disabled />
+      </Form.Group>
+    
+    </>
   );
 }
 
