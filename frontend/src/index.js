64f95b786ca99dbb073c1a2bae2030c946ec8bde
jss3tg <jss3tg@virginia.edu>
2022-06-08 11:14:11 -0400

    heroku x4

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,13 +7,18 @@ import reportWebVitals from './reportWebVitals';
 //import './bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+try{
+  root.render(
   <React.StrictMode>
     <Router>
     <App />
     </Router>
   </React.StrictMode>
 );
+}
+catch (error){
+  console.log(error)
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
