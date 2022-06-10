fcb10bd989fc4bb972a87cbf72e22e7dba8b9013
jss3tg <jss3tg@virginia.edu>
2022-06-10 09:25:49 -0400

    heroku check

--- frontend/src/components/SellerDashboard.js
+++ frontend/src/components/SellerDashboard.js
@@ -1,34 +1,97 @@
-import React from "react";
-import Card from 'react-bootstrap/Card';
+import React, {useContext, useState, useEffect} from "react";
 import Button from 'react-bootstrap/Button';
-import Container from 'react-bootstrap/Container'
+import {UserIDContext} from "../UserIDContext";
 import Form from 'react-bootstrap/Form';
 import './SellerDashboard.css';
 import axios from 'axios';
 
 function SellerDashboard() {
+  const {curUserID} = useContext(UserIDContext);
+  const [currUserData, setCurrUserData] = useState();
+  const [currProductData, setCurrProductData] = useState();
+  useEffect(() => {
+    async function getUser() {
+      const response = await fetch("http://localhost:9000/users/" + curUserID);
+      const body = await response.json();
+      setCurrUserData(body);
+      console.log(body)
+    }
+    getUser();
+  }, []);
+  console.log(currUserData)
+  //console.log(currUserData.result[0]) //displays product IDs under user (when logged in)
+
+  //need to change this function to retrieve each product under currUserData
+  useEffect(() => {
+    if (currUserData){
+      let products = [];
+      currUserData.forEach((item) => {
+        fetch("http://localhost:9000/products/info" + item.id)
+        .then((res) => res.json())
+        .then((data) =>
+        {products.push({...data})}
+      )
+      })
+      setCurrProductData(products)
+      console.log(products)
+    }
+    
+    else{
+      fetch("http://localhost:9000/users/" + curUserID)
+      .then((res) => res.json())
+      .then((data) => setCurrUserData(data.result))
+    }
+  }, [])
+  console.log(currProductData)
+
+
+  
+  const ProductDisplay = (props) => (
+    <section>
+      <div className = 'checkoutPage'>
+        <div className="product">
+          <div className="description">
+          <h4>{props.id}</h4>
+          </div>
+        </div>
+      </div>
+    </section>
+    
+  );
   const onSubmit = (e) => {
     e.preventDefault();
     const time = Date.now()/1000;
+    const pricing = parseFloat(e.target.price.valueAsNumber).toFixed(2)
     const obj = {
       name: e.target.product.value,
       description: e.target.description.value,
       condition: e.target.condition.value,
-      price: e.target.price.valueAsNumber,
+      price: parseFloat(pricing),
       numberAvailable: e.target.numberAvailable.value,
       pickupLocation: e.target.pickupLocation.value,
       negotiable: false,
       datePosted: time,
-      bought:false,
+      bought: false,
     };
+
     submitProduct(obj);
-    console.log(obj)
   };
   const submitProduct = (post) => {
-    axios.post("http://localhost:9000/products/create", post);
+    var postID;
+    console.log(post.name)
+    axios.post("http://localhost:9000/products/create", post).then((res)=> {
+      postID = res.data;
+      axios.put("http://localhost:9000/users/addProduct/"+curUserID+"/"+postID +"/")
+    })
   };
+
   return (
     <>
+    {/* Displays the IDs here, uncomment this*/}
+    {/* {currProductData.length > 0 &&
+          currProductData.map((val, key) => {
+            return <ProductDisplay id={val.id} />;
+          })} */}
     <Form onSubmit={onSubmit}>
       <Form.Group className="mb-3">
         <Form.Label>Name Of Product</Form.Label>
@@ -50,7 +113,7 @@ function SellerDashboard() {
       </Form.Group>
       <Form.Group className="mb-3">
         <Form.Label>Listing Price</Form.Label>
-        <Form.Control placeholder="Price ($)" name="price" type="number" min="1" step="any"/>
+        <Form.Control placeholder="Price ($)" name="price" type="number" min="0.01" step="any"/>
       </Form.Group>
       <Form.Group className="mb-3">
         <Form.Label>Units Available</Form.Label>
